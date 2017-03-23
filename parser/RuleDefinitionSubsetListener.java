b172aacd3b0f39bf0787bc09b8cd6a6a8df7ff6b
Daniel <danielnaveda88@gmail.com>
2017-03-23 10:51:21 +0100

    Improving the tree walker

--- parser/RuleDefinitionSubsetListener.java
+++ parser/RuleDefinitionSubsetListener.java
@@ -19,9 +19,31 @@ public class RuleDefinitionSubsetListener extends RuleDefinitionBaseListener {
     @Override
     public void enterPredicate_body(RuleDefinitionParser.Predicate_bodyContext ctx) {
       // obj.remove("predicates");
-
       JSONArray list = (JSONArray) obj.get("predicates");
       list.add("New entry");
+    }
+
+    @Override
+    public void enterEmit(RuleDefinitionParser.EmitContext ctx) {
+      JSONObject event_t_obj = (JSONObject) obj.get("event_template");
+      event_t_obj.put("ty_id", ctx.CAPITAL_IDENTIFIER().getText());
+      event_t_obj.put("attributes", new JSONArray());
+    }
+
+    @Override
+    public void enterEvaluation(RuleDefinitionParser.EvaluationContext ctx) {
+      JSONObject event_t_obj = (JSONObject) obj.get("event_template");
+      JSONArray attributes = (JSONArray) event_t_obj.get("attributes");
+
+      JSONObject parameter = new JSONObject();
+      JSONObject parameter_content = new JSONObject();
+
+      parameter_content.put("predicate", new Integer(0));
+      parameter_content.put("parameter", new Integer(0));
+
+      parameter.put("Parameter", parameter_content);
+
+      attributes.add(parameter);
     }
 
     @Override
