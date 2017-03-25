0107be6735719077a000fefcbafa8208775be697
Daniel <danielnaveda88@gmail.com>
2017-03-25 23:37:33 +0100

    Completing some operations for the testing of a fire event from scratch

--- scripts/tools/testing_fire_from_scratch.sh
+++ scripts/tools/testing_fire_from_scratch.sh
@@ -12,3 +12,20 @@ printf "================= Testing TRex ================="; echo
 
 # ../operations/8_define_rule.sh "from 0[x = 0]() as SMK and last 1[y = 1](0 == x, 1 > 45) as TEMP within 5min from SMK emit 2(0 = x, 1 = y)"
 ../operations/8_define_rule.sh "from 0[x = 0]() as smk and last 1[y = 1](0 == x, 1 > 45) as temp within 5min from smk emit 2(0 = x, 1 = y)"
+
+# Get connection ID
+OUTPUT=$(curl --fail --silent --show-error http://127.0.0.1:8888/connections)
+CONN_ID=$(echo $OUTPUT | jq -r '.value')
+
+# The subscription ID is not taken into account because the engine does not distinguish event types
+../operations/2_subscribe.sh $CONN_ID 0
+
+# Temperature
+curl -H "Content-Type: application/json" -X POST -d \
+"{\"tuple\": {\"ty_id\": \"1\",\"data\": [\"area_1\", \"50\"]},\"time\": \"2016-12-12T09:51:03.570254485Z\"}"\
+ http://127.0.0.1:8888/events; echo; echo;
+
+# Smoke
+curl -H "Content-Type: application/json" -X POST -d \
+"{\"tuple\": {\"ty_id\": \"0\",\"data\": [\"area_1\"]},\"time\": \"2016-12-12T09:51:03.570254485Z\"}"\
+ http://127.0.0.1:8888/events; echo; echo;
